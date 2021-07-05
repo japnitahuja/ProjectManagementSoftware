34b3d1091994c8c372d5b793b0f09de63397688a
krishna-kedia <62465437+krishna-kedia@users.noreply.github.com>
2021-07-05 21:21:33 +0530

    fixed organisation bug.

--- frontend/src/components/task-nav/task-nav.component.jsx
+++ frontend/src/components/task-nav/task-nav.component.jsx
@@ -1,14 +1,21 @@
+import React from 'react'
+import { connect, useSelector } from 'react-redux'
+import { createStructuredSelector } from "reselect";
+import { selectCurrentOrganisationId } from "../../redux/orgnaisation/organisation.selectors";
 import {Link} from "react-router-dom";
 import { SearchDiv, NavBar, Heading, Back, Image, OneThirdDiv } from "./task-nav.styles"
 import backArrow from "../../assets/backArrowBlack.png"
 import search from "../../assets/navsearch.png"
+  
 
-export const TaskNav = ({title, toggleSearch}) => {
+export const TaskNav = ({title, toggleSearch, orgId}) => {
+    const org = useSelector(state => state.organisation)
+    console.log(org.organisationId)
     return (
         <NavBar>
             <OneThirdDiv align="flex-start">
             <Back>
-                <Link to = "/all-projects" style={{textDecoration:'none', color:'black'}}> 
+                <Link to ={`/all-projects/${org.organisationId}`} style={{textDecoration:'none', color:'black'}}> 
                     <Image src={backArrow}/> Projects
                 </Link>
             </Back>
@@ -26,3 +33,7 @@ export const TaskNav = ({title, toggleSearch}) => {
         </NavBar>
     )
 }
+
+export default TaskNav
+
+
